0c43a235b1045a0141025cf7e7691eb540e6fb7f
ghardard <ghardard@gmail.com>
2013-10-28 18:27:28 +0000

    Added test file, and other stuff.

--- src/main/java/ru/HelloWorld.java
+++ src/main/java/ru/HelloWorld.java
@@ -2,6 +2,7 @@ package ru.helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		World world = new World();
 		System.out.println("Hello world");
 	}
 
---
+++ src/main/java/ru/World.java
@@ -0,0 +1,8 @@
+package ru.helloworld;
+
+public class World {
+	public String greet(){
+		return "Hello world!";
+	}
+
+}
