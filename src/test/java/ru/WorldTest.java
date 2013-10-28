710c3c5f0fa6b0db6f3573377ca1ccc8de69fdff
ghardard <ghardard@gmail.com>
2013-10-28 21:33:14 +0000

    Changed WorldTest.java.

--- src/test/java/ru/WorldTest.java
+++ src/test/java/ru/WorldTest.java
@@ -1,11 +1,11 @@
 package ru.helloworld;
 
-import org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class WorldTest {
-	
+
 	@Test
 	public void greetResultsInHello() {
 		World world = new World();
